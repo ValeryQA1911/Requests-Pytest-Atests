f0edd9ba1532392a5d0161bf205b15f98cf5f9f7
Valery_Dymchenko <vaiera.com@mail.ru>
2025-04-23 01:42:01 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+import requests
+
+trainer_token = 'your_token'
+
+URL = 'https://api.pokemonbattle.ru'
+HEADER = {'Content-Type' : 'application/json', 
+          'trainer_token' : 'your_token'}
+
+create_body = {
+    "name": "Кот",
+    "photo_id": -1
+}
+
+response_create = requests.post (url = f'{URL}/v2/pokemons', headers = HEADER ,  json = create_body)
+print (response_create.text)
+
+resp_pok_id = response_create.json() ['id']
+
+change_body = {
+    'pokemon_id': resp_pok_id,
+    'name': "Пес",
+    'photo_id': -1
+}
+
+response_change = requests.put (url = f'{URL}/v2/pokemons', headers = HEADER ,  json = change_body)
+print (response_change.text)
+
+add_pok_resp = requests.post (url = f'{URL}/v2/trainers/add_pokeball', headers = HEADER ,  json = {'pokemon_id' : resp_pok_id})
+
+print (add_pok_resp.text)
---
+++ tests/test_pokemon.py
@@ -0,0 +1,18 @@
+import requests
+import pytest
+
+trainer_token = 'your_token'
+URL = 'https://api.pokemonbattle.ru/v2'
+HEADER = {'Content-Type' : 'application/json', 
+          'trainer_token' : trainer_token}
+trainer_id = '28614'
+trainer_name = 'HardStyle'
+
+def test_get_trainers():
+    trainers_response = requests.get (url = f'{URL}/trainers', headers = HEADER)
+    assert trainers_response.status_code == 200
+
+
+def test_get_trainer_name():
+    responce_get = requests.get (url = f'{URL}/trainers', headers = HEADER, params =  {'trainer_id' : trainer_id})
+    assert responce_get.json()['data'][0]['trainer_name'] == trainer_name
